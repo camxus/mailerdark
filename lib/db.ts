a23cddcd8723129fffef000b750f14f5a2eaedd8
camxus <camillus.konkwo@gmail.com>
2026-07-24 18:39:42 +0200

    Increase Prisma connection pool to fix pool timeout errors

--- lib/db.ts
+++ lib/db.ts
@@ -8,6 +8,7 @@ export const db =
   globalForPrisma.prisma ??
   new PrismaClient({
     log: process.env.NODE_ENV === "development" ? ["warn", "error"] : ["error"],
+    pool: { min: 2, max: 10 },
   });
 
 if (process.env.NODE_ENV !== "production") {
